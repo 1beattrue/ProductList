52a47053adeecfc4007224824438b7fca0117c4a
Vitaly <onebeattrue@gmail.com>
2024-03-10 18:18:24 +0300

    Реализовал DetailsContent

--- app/src/main/java/edu/mirea/onebeattrue/productlist/presentation/details/DetailsContent.kt
+++ app/src/main/java/edu/mirea/onebeattrue/productlist/presentation/details/DetailsContent.kt
@@ -1,13 +1,158 @@
 package edu.mirea.onebeattrue.productlist.presentation.details
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import edu.mirea.onebeattrue.productlist.R
+import edu.mirea.onebeattrue.productlist.domain.entity.Product
+import edu.mirea.onebeattrue.productlist.presentation.extensions.formattedPrice
+import edu.mirea.onebeattrue.productlist.presentation.extensions.roundedRating
+import edu.mirea.onebeattrue.productlist.presentation.products.RatingBar
+
 
 @Composable
 fun DetailsContent(
     modifier: Modifier = Modifier,
     component: DetailsComponent
 ) {
-    Text(text = "details")
+    val state by component.model.collectAsState()
+
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        topBar = {
+            TopBar(
+                onBackClick = { component.onClickBack() }
+            )
+        }
+    ) { paddingValues ->
+        DetailsCard(
+            paddingValues = paddingValues,
+            product = state.product
+        )
+    }
+}
+
+@OptIn(ExperimentalGlideComposeApi::class, ExperimentalFoundationApi::class)
+@Composable
+private fun DetailsCard(
+    modifier: Modifier = Modifier,
+    paddingValues: PaddingValues,
+    product: Product
+) {
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(paddingValues)
+    ) {
+        item {
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(256.dp),
+            ) {
+                items(
+                    items = product.images
+                ) { image ->
+                    GlideImage(
+                        modifier = Modifier.animateItemPlacement(),
+                        model = image,
+                        contentDescription = null,
+                        contentScale = ContentScale.FillHeight
+                    )
+                }
+            }
+        }
+        item {
+            ProductDescription(product = product)
+        }
+    }
+}
+
+@Composable
+private fun ProductDescription(
+    modifier: Modifier = Modifier,
+    product: Product
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = product.title,
+            style = MaterialTheme.typography.headlineLarge
+        )
+        RatingBar(
+            rating = product.rating.roundedRating()
+        )
+        Text(
+            text = product.brand,
+            style = MaterialTheme.typography.bodyLarge
+        )
+        Text(
+            modifier = Modifier
+                .align(Alignment.End),
+            text = product.price.formattedPrice(),
+            style = MaterialTheme.typography.headlineMedium,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(R.string.description_title),
+            style = MaterialTheme.typography.bodyLarge,
+            fontWeight = FontWeight.SemiBold
+        )
+        Text(
+            text = product.description,
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun TopBar(
+    modifier: Modifier = Modifier,
+    onBackClick: () -> Unit
+) {
+    CenterAlignedTopAppBar(
+        modifier = modifier,
+        title = { Text(text = stringResource(R.string.details_title)) },
+        navigationIcon = {
+            IconButton(onClick = { onBackClick() }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
+                    contentDescription = null
+                )
+            }
+        }
+    )
 }
